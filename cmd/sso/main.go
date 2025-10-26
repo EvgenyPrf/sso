ff5bb562e9f5b2ab54c2c93c8e2cbc41f94fe0ff
evgenyparfenyuk <evgenii.parfeniuk@onlinesup.com>
2025-10-26 21:51:48 +0300

    add config

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,7 +1,15 @@
-package sso
+package main
+
+import (
+	"fmt"
+	"sso/internal/config"
+)
 
 func main() {
-	//TODO: инициализировать объект конфига
+	//инициализировать объект конфига
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
 
 	//TODO: инициализировать логгер
 
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,15 +1,58 @@
 package config
 
-import "time"
+import (
+	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+	"time"
+)
 
 type Config struct {
 	Env         string        `yaml:"env" env-default:"local"`
 	StoragePath string        `yaml:"storage_path" env-required:"true"`
-	TokenTTL    time.Duration `yaml:"token_ttl" env-default:"2h"`
-	GRPC        GRPCConfig
+	TokenTTL    time.Duration `yaml:"token_ttl" env-required:"true"`
+	GRPC        GRPCConfig    `yaml:"grpc"`
 }
 
 type GRPCConfig struct {
 	Port    int           `yaml:"port"`
 	Timeout time.Duration `yaml:"timeout"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+
+	if path == "" {
+		panic("config path is empty!")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file does not exist: " + path)
+	}
+
+	var config Config
+
+	if err := cleanenv.ReadConfig(path, &config); err != nil {
+		panic("failed to read config " + err.Error())
+	}
+
+	return &config
+}
+
+// получение информации о пути файла конфига из двух возможных источников: либо из переменных окружения, либо из флага
+// Priority: flag > env > default.
+// Default value is empty string.
+func fetchConfigPath() string {
+	var res string
+
+	//указание пути файлв конфигурации при запуске приложения
+	//пример: --config="path/to/config.yaml"
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
 }
