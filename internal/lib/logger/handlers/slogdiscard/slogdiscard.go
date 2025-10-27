3ac76c1e631dd34e210cfb48e2e75921b3ae4dc2
evgenyparfenyuk <evgenii.parfeniuk@onlinesup.com>
2025-10-27 10:40:29 +0300

    add discard logger for tests

--- internal/lib/logger/handlers/slogdiscard/slogdiscard.go
+++ internal/lib/logger/handlers/slogdiscard/slogdiscard.go
@@ -1,6 +1,35 @@
 package slogdiscard
 
+import (
+	"context"
+	"log/slog"
+)
+
 //TODO: реализовать интерфейс слога для тестов, методы не должны ничего делать
 
+func NewDiscardLogger() *slog.Logger {
+	return slog.New(NewDiscardHandler())
+}
+
 type DiscardHandler struct {
+}
+
+func NewDiscardHandler() *DiscardHandler {
+	return &DiscardHandler{}
+}
+
+func (h *DiscardHandler) Enabled(_ context.Context, _ slog.Level) bool {
+	return false
+}
+
+func (h *DiscardHandler) Handle(_ context.Context, _ slog.Record) error {
+	return nil
+}
+
+func (h *DiscardHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return h
+}
+
+func (h *DiscardHandler) WithGroup(name string) slog.Handler {
+	return h
 }
